841280684a6832e89ee14cdcc0f8884536754f31
NhatMinh0208 <minhkhicon2468@gmail.com>
2023-02-25 10:32:37 +0700

    Added API description

--- backend/constants.js
+++ backend/constants.js
@@ -1,6 +1,20 @@
-export const ACTIONS = {
+export const ACTION = {
     SET_USERNAME: 'setUserName',
     CREATE_ROOM: 'createRoom',
     JOIN_ROOM: 'joinRoom',
-    PLAY_ACTION: 'playAction'
+    PLAY_MOVE: 'playAction'
+}
+
+export const RESPONSE_TYPE = {
+    ERROR: 'error',
+    OK: 'ok',
+    ROOM_CODE: 'roomCode',
+    GAME_LOG: 'gameLog',
+    GAME_STATE: 'gameState'
+}
+
+export const MOVE_TYPE = {
+    DRAW: 'draw',
+    PLAY_SPELL: 'playSpell',
+    END_TURN: 'endTurn'
 }
