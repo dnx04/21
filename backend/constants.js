8fc0588795934ca065199e5dbaecce426b2763da
NhatMinh0208 <minhkhicon2468@gmail.com>
2023-02-25 17:48:55 +0700

    First draft of spell cards to be included

--- backend/constants.js
+++ backend/constants.js
@@ -23,7 +23,25 @@ const MOVE_TYPE = {
 };
 
 const SPELL = {
-    
+    ONE_UP: "One-Up",  // Increases attack by 1, draw 1 spell (4)
+    TWO_UP: "Two-Up",  // Increases attack by 2, return opponent's last drawn card (2)
+    DRAW_3: "Draw 3",  // Draw the 3 card (2)
+    DRAW_4: "Draw 4",  // Draw the 4 card (2)
+    DRAW_5: "Draw 5",  // Draw the 5 card (2)
+    DRAW_6: "Draw 6",  // Draw the 6 card (2)
+    DRAW_7: "Draw 7",  // Draw the 7 card (2)
+    REMOVE: "Remove",  // Return opponent's last drawn card (4)
+    RETURN: "Return",  // Return player's last drawn card (4)
+    EXCHANGE: "Exchange", // Swap the players' last drawn face-up card (3)
+    TRADE: "Trade", // Trade 2 other spells at random for 3 from the deck, cannot be used if under 3 (3)
+    GREED: "Greed", // Increase attack by (number of opponent spells in hand)/2 rounded up (2)
+    BANISH: "Banish", // Remove all opponent spells currently on the table (3)
+    CHANGE_17: "Change-Up 17", // Change target score to 17 (3)
+    CHANGE_21: "Change-Up 21", // Change target score to 21 (3)
+    CHANGE_24: "Change-Up 24", // Change target score to 24 (3)
+    PERFECT_DRAW: "Perfect Draw", // Draw the best possible card from the deck (3)
+    SCRAP: "Scrap", // Cancels current round without a winner (4)
+    PERFECT_EXECUTION: "Perfect Execution", // Deals 10 damage at the end of a round if you have exactly the target score (2)
 }
 
 const ERROR = {
